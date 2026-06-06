790359766fb21f06ccf17f6b730fe030adf1af8c
jorhisbigeard-cyber <jorhisbigeard@gmail.com>
2026-06-06 12:36:51 +0200

    feat: add lava.link node

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ client.lavalink = new LavalinkManager({
   nodes: [
     { host: 'lava-v4.ajieblogs.eu.org', port: 80, authorization: 'https://dsc.gg/ajidevserver', secure: false, id: 'node1', retryAmount: 5, retryDelay: 10000 },
     { host: 'lavalink.jirayu.net', port: 13592, authorization: 'youshallnotpass', secure: false, id: 'node3', retryAmount: 5, retryDelay: 10000 },
+    { host: 'lava.link', port: 80, authorization: 'discloud', secure: false, id: 'node4', retryAmount: 5, retryDelay: 10000 },
   ],
   sendToShard: (guildId, payload) => {
     const guild = client.guilds.cache.get(guildId);
