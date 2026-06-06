57b5ed278d0ae9052270680b5704a80e12efd78e
jorhisbigeard-cyber <jorhisbigeard@gmail.com>
2026-06-06 13:33:02 +0200

    fix: increase request timeout for HF node

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ for (const file of commandFiles) {
 
 client.lavalink = new LavalinkManager({
   nodes: [
-    { host: 'jorhis-musicoflow-lavalink.hf.space', port: 443, authorization: 'musicoflow', secure: true, id: 'hf-node', retryAmount: 5, retryDelay: 10000 },
+    { host: 'jorhis-musicoflow-lavalink.hf.space', port: 443, authorization: 'musicoflow', secure: true, id: 'hf-node', retryAmount: 5, retryDelay: 10000, requestTimeout: 30000 },
   ],
   sendToShard: (guildId, payload) => {
     const guild = client.guilds.cache.get(guildId);
